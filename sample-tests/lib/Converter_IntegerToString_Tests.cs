c268db80d9894850e57230900dba417f535c9875
Mark Kochanski <markk@uw.edu>
2023-01-19 15:47:30 -0800

    Remove build files

---
+++ sample-tests/lib/Converter_IntegerToString_Tests.cs
@@ -0,0 +1,14 @@
+namespace sample_tests;
+using Xunit;
+using sample.lib;
+
+public class Convert_IntegerToString_Tests
+{
+    [Fact]
+    public void SimpleOne()
+    {
+        int number = 1;
+        string result = Converter.IntegerToString(number);
+        Assert.Equal(result,"One",true);
+    }
+}
