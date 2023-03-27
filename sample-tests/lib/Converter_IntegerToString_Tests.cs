3e54005e862b35820892c1a5a3b941ed6ac583bb
Mark Kochanski <markk@uw.edu>
2023-03-27 16:08:03 -0700

    Updates to the project to improve it for practicing TDD.

--- sample-tests/lib/Converter_IntegerToString_Tests.cs
+++ sample-tests/lib/Converter_IntegerToString_Tests.cs
@@ -2,13 +2,22 @@ namespace sample_tests;
 using Xunit;
 using sample.lib;
 
-public class Convert_IntegerToString_Tests
+public class Convert_IntegerToWordedString_Tests
 {
     [Fact]
-    public void SimpleOne()
+    //TODO: This test will become obsolete if the featured function is fully implemented.
+    public void NotImplementedYet()
+    {
+        Action act = () => Converter.IntegerToWordedString(int.MinValue);
+        Assert.Throws<NotImplementedException>(act);
+    }
+
+    [Fact]
+    //TODO: Initial "red" test to code logic
+    public void One()
     {
         int number = 1;
-        string result = Converter.IntegerToString(number);
-        Assert.Equal("One",result,true);
+        string words = Converter.IntegerToWordedString(number);
+        Assert.Equal("one",words,true);
     }
 }
