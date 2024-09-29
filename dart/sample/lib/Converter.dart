0e2ddd0cb86c16740615dabc87898668bacc5fc9
Victor Long <nackor91@gmail.com>
2024-09-29 00:42:09 -0700

    tens test pass

--- dart/sample/lib/Converter.dart
+++ dart/sample/lib/Converter.dart
@@ -1,5 +1,8 @@
 
 String integerToWordedString(int number) {
-  return "one";
-
+  final ones = <String>['zero','one','two','three','four','five','six','seven','eight','nine'];
+  final tens = <String>['ten','eleven','twleve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen'];
+  if (number < 10){return 'one';}
+  if (number < 20){return 'ten';}
+  return 'woops';
 }
