83ef2d80c4fd009630550a710e281c0501b565c1
Victor Long <nackor91@gmail.com>
2024-09-29 01:26:25 -0700

    ninety-nine test passses

--- dart/sample/lib/Converter.dart
+++ dart/sample/lib/Converter.dart
@@ -1,8 +1,10 @@
 
 String integerToWordedString(int number) {
   final ones = <String>['zero','one','two','three','four','five','six','seven','eight','nine'];
-  final tens = <String>['ten','eleven','twleve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen'];
-  if (number < 10){return 'one';}
-  if (number < 20){return 'ten';}
+  final teens = <String>['ten','eleven','twleve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen'];
+  final tens = <String>['zeroth','ten','twenty','thirty','fourty','fifty','sixty','seventy','eighty','ninety'];
+  if (number < 10){return ones[number];}
+  if (number < 20){return teens[number%10];}
+  if (number < 100){return tens[(number / 10).floor()] + '-' +  integerToWordedString(number % 10);}
   return 'woops';
 }
