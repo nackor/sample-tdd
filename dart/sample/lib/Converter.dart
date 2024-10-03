e22eddd400ce53b94942eda360d5365ae2189f06
Victor Long <nackor91@gmail.com>
2024-10-03 00:32:04 -0700

    zero unit works along with other tests

--- dart/sample/lib/Converter.dart
+++ dart/sample/lib/Converter.dart
@@ -3,6 +3,8 @@ String integerToWordedString(int number) {
   final ones = <String>['','one','two','three','four','five','six','seven','eight','nine'];
   final teens = <String>['ten','eleven','twleve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen'];
   final tens = <String>['zeroth','ten','twenty','thirty','fourty','fifty','sixty','seventy','eighty','ninety'];
+
+  if (number == 0) {return "zero";}
   if (number < 10){return ones[number];}
   if (number < 20){return teens[number%10];}
   if (number < 100){return tens[(number / 10).floor()] + '-' +  integerToWordedString(number % 10);}
