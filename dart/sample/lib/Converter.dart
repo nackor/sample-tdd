9e771fecad9df14d467c345f67e9df9b4e38e590
Victor Long <nackor91@gmail.com>
2024-10-03 03:00:47 -0700

    fixed forty spelling

--- dart/sample/lib/Converter.dart
+++ dart/sample/lib/Converter.dart
@@ -7,13 +7,13 @@ String integerToWordedString(int number) {
 String recurse_integerToWordedString(int number) {
   final ones = <String>['','one','two','three','four','five','six','seven','eight','nine'];
   final teens = <String>['ten','eleven','twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen'];
-  final tens = <String>['zeroth','ten','twenty','thirty','fourty','fifty','sixty','seventy','eighty','ninety'];
+  final tens = <String>['zeroth','ten','twenty','thirty','forty','fifty','sixty','seventy','eighty','ninety'];
   if (number < 10){return ones[number];}
   if (number < 20){return teens[number % 10];}
   if (number < 100){return tens[(number / 10).floor()] + ( (number%10==0) ? "" : '-' + recurse_integerToWordedString(number % 10));}
   if (number < 1000){return ones[(number/100).floor()] + " hundred" + ( (number%100==0) ? "" : " " + recurse_integerToWordedString(number % 100));}
   if (number<1000000){return  recurse_integerToWordedString(number ~/ 1000) + ' thousand' + ( (number%1000==0) ? "" : " " + recurse_integerToWordedString(number % 1000));}
-  if (number<10000000){return  recurse_integerToWordedString(number ~/ 1000000) + ' million' + ( (number%1000000==0) ? "" : " " + recurse_integerToWordedString(number % 1000000));}
+  if (number<100000000){return  recurse_integerToWordedString(number ~/ 1000000) + ' million' + ( (number%1000000==0) ? "" : " " + recurse_integerToWordedString(number % 1000000));}
 
   return 'woops';
 }
